c04030fdcb170b4eb39d53647c01b7657229cfa4
Cheney <cheney@192-168-1-9.tpgi.com.au>
2018-06-11 11:45:17 +1000

    Add test file.

---
+++ project-prototypes/java/core/maven/src/test/java/com/cyberitcamp/prototypes/java/core/MainTest.java
@@ -0,0 +1,10 @@
+package com.cyberitcamp.prototypes.java.core;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testTemp() {
+        assertEquals(2, 1 + 1);
+    }
+}
