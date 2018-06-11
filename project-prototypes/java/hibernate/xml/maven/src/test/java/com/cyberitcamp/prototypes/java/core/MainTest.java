6cea9433d596b554fb117e96fb5e5e65d4ced9fe
Cheney <cheney@192-168-1-9.tpgi.com.au>
2018-06-11 21:02:53 +1000

    Add hibernate prototypes.

---
+++ project-prototypes/java/hibernate/xml/maven/src/test/java/com/cyberitcamp/prototypes/java/core/MainTest.java
@@ -0,0 +1,12 @@
+package com.cyberitcamp.prototypes.java.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void testTemp() {
+        assertEquals(2, 1 + 1);
+    }
+}
