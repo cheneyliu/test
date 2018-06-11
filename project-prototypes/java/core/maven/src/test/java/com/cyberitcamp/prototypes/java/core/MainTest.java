489ed2d1c57c6094f74a0012a5793bf179dc27ab
Cheney <cheney@192-168-1-9.tpgi.com.au>
2018-06-11 17:07:03 +1000

    Add missing import statement.

--- project-prototypes/java/core/maven/src/test/java/com/cyberitcamp/prototypes/java/core/MainTest.java
+++ project-prototypes/java/core/maven/src/test/java/com/cyberitcamp/prototypes/java/core/MainTest.java
@@ -2,6 +2,8 @@ package com.cyberitcamp.prototypes.java.core;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
     @Test
     void testTemp() {
